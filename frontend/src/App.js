114c9f6a0b8a13bfc7ca81416c7a3143f30909b6
Hong Ma <hong.ma@rea-group.com>
2020-05-04 16:59:51 +0800

    completed the fronted and docker file docker-compose

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,63 @@
-import React,{ useEffect, useState } from 'react';
-import  axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
   const [books, setBooks] = useState([]);
+  const [bookName, setBookName] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const fetchData = async () => {
+    const result = await axios(
+      '/books/',
+    );
+    setBooks(result.data);
+  };
 
   useEffect(() => {
-    const fetchData = async () => {
-      const result = await axios(
-        './books/',
-      );
-      console.log(result,"result");
-
-      setBooks(result.data);
-    };
-
     fetchData();
-  },[]);
+  }, []);
+
+  const handleSubmit = () => {
+    axios.post('/books/', {
+      name: bookName,
+      author: author
+    })
+      .then(function (response) {
+        console.log(response);
+        setBookName('');
+        setAuthor('');
+        fetchData();
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
 
   return (
     <div className="App">
       <header className="App-header">
+        <div>
+          <h2>Add new Book</h2>
+          <div>
+            <label className='label'>Please input book name:</label>
+            <input className='input'
+                   value={bookName}
+                   onChange={(e) => setBookName(e.target.value)}/>
+          </div>
+          <div>
+            <label className='label'>Please inout author:</label>
+            <input className='input'
+                   value={author}
+                   onChange={(e) => setAuthor(e.target.value)}
+            />
+          </div>
+          <button className='submit' onClick={() => handleSubmit()}> submit</button>
+        </div>
         <h2>book list</h2>
         <div>
-          {books.map((book)=>{
-            return (<li>{book.name}</li>)
+          {books.map((book) => {
+            return (<li key={book.id}>{book.name}</li>);
           })}
         </div>
       </header>
